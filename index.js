4690ecdb355a3ca8bd54cc55b8ba09942b759933
omarfaruk70 <faruk1611281892@gmail.com>
2023-12-22 23:15:43 +0600

    having issue with queryparameter sort by price and filter by category

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
 require('dotenv').config()
+const app = express()
 const port = process.env.PORT || 5000
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true 
+}));
 app.use(express.json());
-app.use(cors());
 
 
 
@@ -23,17 +26,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db('Dines-Junction-Client');
+    const database = client.db('Dines-Junction');
     const allFoodCollection = database.collection('all food');
+    const purchaseFoodCollection = database.collection('purchased foods');
 
 
     // get all food in the all food routes
+    // 1. ----> sortbyquery(origin/country)
+    // demo link: http://localhost:5000/api/v1/route/getallfood?origin='Bangladesh' eivabe request korle specific origin er data dibe
+    // ar na hole sob dibe.. eita ekta object er maddhome pathate hoy.
+    // 2. ---> sort by accending / or decending
     app.get('/api/v1/route/getallfood', async(req, res) => {
+
+      let queryObj = {};  // filter by country(bangladesh, india, china, america)
+      const sortObj = {};
+
+
+      const origin = req.query.origin;
+      const sortField = req.query.sortField;
+      const sortOrder = req.query.sortOrder;
+
       const options = {
-        sort: {price: 1},
-        projection : {_id: 1, foodCategory: 1, foodImage: 1, foodName: 1, price: 1, quantity: 1}
+        projection : {_id: 1, foodCategory: 1, foodImage: 1, foodName: 1, price: 1, origin: 1, quantity: 1}
       }
-      const cursor = allFoodCollection.find({}, options);
+
+      if(origin){
+        queryObj.origin = origin;
+      }
+      if(sortField && sortOrder){
+        sortObj[sortField] = sortOrder;
+      }
+      const cursor = allFoodCollection.find({}, options, queryObj).sort(sortObj);
       const result =await cursor.toArray();
       res.send(result);
     })
@@ -46,7 +69,25 @@ async function run() {
     res.send(result)
   })
 
+   // get a single food item for loading the order/purchase page
+   app.get('/api/v1/route/getallfood/fooditem/:id', async(req, res) => {
+    const foodId = req.params.id;
+    const query = {_id: new ObjectId(foodId)};
+    const options = {
+      sort: {price: 1},
+      projection : {foodName: 1, price: 1, quantity: 1}
+    }
+    const result = await allFoodCollection.findOne(query, options);
+    res.send(result)
+  })
 
+  // post user purchase informations at a collection
+  app.post('/api/v1/user/purchasefood', async(req, res) => {
+    const specificFood = req.body;
+    const result = await purchaseFoodCollection.insertOne(specificFood);
+    // console.log(result);
+    res.send(result)
+  })
 
 
 
