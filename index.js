cfa491c25fab644f36769e00b66db6e2e3fb7a05
omarfaruk70 <faruk1611281892@gmail.com>
2024-01-06 16:42:11 +0600

    added an api of topfood section

--- index.js
+++ index.js
@@ -31,25 +31,24 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("Dines-Junction");
-    const loggedinUser = database.collection('loggedin user');
+    const loggedinUser = database.collection("loggedin user");
     const allFoodCollection = database.collection("all food");
     const purchaseFoodCollection = database.collection("purchased foods");
 
-
-      // jwt middleware -----> if token is exist then enter or logout
-      const logger = (req, res, next) => {
-        const token = req.cookies.access_token;
-        if (!token) {
-          return res.status(401).send({ message: "UnAuthorized Access" });
+    // jwt middleware -----> if token is exist then enter or logout
+    const logger = (req, res, next) => {
+      const token = req.cookies.access_token;
+      if (!token) {
+        return res.status(401).send({ message: "UnAuthorized Access" });
+      }
+      jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decode) => {
+        if (err) {
+          return res.status(403).send({ message: "Forbidden Access" });
         }
-        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decode) => {
-          if (err) {
-            return res.status(403).send({ message: "Forbidden Access" });
-          }
-          req.user = decode;
-          next();
-        });
-      };
+        req.user = decode;
+        next();
+      });
+    };
 
     // // Create a json web token and send it to the client side
     app.post("/api/v1/user/auth/jwt/access_token", async (req, res) => {
@@ -66,8 +65,6 @@ async function run() {
         .send({ success: true });
     });
 
-  
-
     // get all food in the all food routes
     // 1. ----> sortbyquery(origin/country)
     // demo link: http://localhost:5000/api/v1/route/getallfood?origin='Bangladesh' eivabe request korle specific origin er data dibe
@@ -82,7 +79,7 @@ async function run() {
       const allfoodCount = await allFoodCollection.estimatedDocumentCount();
       res.send({ allfoodCount });
     });
-    app.get("/api/v1/route/getallfood",  async (req, res) => {
+    app.get("/api/v1/route/getallfood", async (req, res) => {
       let queryObj = {}; // filter by country(bangladesh, india, china, america)
       const sortObj = {};
 
@@ -121,6 +118,13 @@ async function run() {
       res.send(result);
     });
 
+    // get top selling 6 food item for the topfood section
+    app.get('/api/v1/route/topfood', async(req, res) => {
+      const cursor = allFoodCollection.find().sort({price: 'desc'}).limit(6);
+      const result = await cursor.toArray() ;
+      res.send(result)
+    })
+
     // get a single food item from the allfood page
     app.get("/api/v1/route/getallfood/singlefood/:id", async (req, res) => {
       const foodId = req.params.id;
@@ -149,50 +153,49 @@ async function run() {
 
     // get all orderd food item based on email
     app.get("/api/v1/route/getorderdfood", logger, async (req, res) => {
-    const queryEmail = req.query.email;
-    const tokenEmail = req.user.email;
-    if(queryEmail.toLowerCase() !== tokenEmail.toLowerCase()){
-      return res.status(401).send({message: 'UnAuthorized Access'});
-    }
-    let query = {};
-    if(queryEmail){
-      query.buyerEmail = queryEmail ; // alternative query["buyerEmail"] = queryEmail;
-    }
-    const result = await purchaseFoodCollection.find(query).toArray();
-    res.send(result);
+      const queryEmail = req.query.email;
+      const tokenEmail = req.user.email;
+      if (queryEmail.toLowerCase() !== tokenEmail.toLowerCase()) {
+        return res.status(401).send({ message: "UnAuthorized Access" });
+      }
+      let query = {};
+      if (queryEmail) {
+        query.buyerEmail = queryEmail; // alternative query["buyerEmail"] = queryEmail;
+      }
+      const result = await purchaseFoodCollection.find(query).toArray();
+      res.send(result);
     });
 
     // get my added food based on user login email / jwt token email
-    app.get('/api/v1/route/myaddedfood',logger, async(req, res) => {
+    app.get("/api/v1/route/myaddedfood", logger, async (req, res) => {
       const queryEmail = req.query.email;
       const tokenEmail = req.user.email;
-      if( !queryEmail || (queryEmail !== tokenEmail)){
-       return res.status(401).send({message: 'UnAuthorized Access'})
-      }
-      else{
+      if (!queryEmail || queryEmail !== tokenEmail) {
+        return res.status(401).send({ message: "UnAuthorized Access" });
+      } else {
         let query = {};
-        if(queryEmail){
-          query.email = queryEmail;
+        if (queryEmail) {
+          query.userEmail = queryEmail;
         }
         const cursor = allFoodCollection.find(query);
-        const result = await cursor.toArray() ;
+        const result = await cursor.toArray();
         res.send(result);
       }
-    })
+    });
 
     // get a single food of myaddedfood for update specific food information
-    app.get('/api/v1/route/myaddedfood/:id', async(req, res) => {
+    app.get("/api/v1/route/myaddedfood/:id", async (req, res) => {
       const foodId = req.params.id;
-      const query = {_id: new ObjectId(foodId)};
+      const query = { _id: new ObjectId(foodId) };
       const result = await allFoodCollection.findOne(query);
       res.send(result);
-    })
+    });
     // trac all loggedin / registerd user in database
-    app.post('/api/v1/savealluserinfo/loggedinuser', async(req, res) => {
+    app.post("/api/v1/savealluserinfo/loggedinuser", async (req, res) => {
       const userInfo = req.body;
       const result = await loggedinUser.insertOne(userInfo);
       res.send(result);
-    })
+    });
     // post user purchase informations at a collection
     app.post("/api/v1/user/purchasefood", logger, async (req, res) => {
       const specificFood = req.body;
@@ -201,47 +204,46 @@ async function run() {
     });
 
     // post new add food on the allfoodCollection with AddBy(logged in userName and Email);
-    app.post('/api/v1/user/addfood', logger, async(req, res)=> {
+    app.post("/api/v1/user/addfood", logger, async (req, res) => {
       const thisFood = req.body;
       const result = await allFoodCollection.insertOne(thisFood);
       res.send(result);
-    })
+    });
 
     // Put or update a addedfood information
-    app.put('/api/v1/user/updatefoodinfo/:id', async(req, res) => {
+    app.put("/api/v1/user/updatefoodinfo/:id", async (req, res) => {
       const specificFoodId = req.params.id;
       const specificFood = req.body;
-      const filter = {_id: new ObjectId(specificFoodId)}
+      const filter = { _id: new ObjectId(specificFoodId) };
       const updatedInfo = {
         $set: {
           foodName: specificFood.foodName,
           foodImage: specificFood.foodImage,
-          foodCategory:specificFood.foodCategory,
+          foodCategory: specificFood.foodCategory,
           origin: specificFood.origin,
           price: specificFood.price,
           ingredients: specificFood.ingredients,
-          quantity: specificFood.quantity
-        }
-      }
+          quantity: specificFood.quantity,
+        },
+      };
       const result = await allFoodCollection.updateOne(filter, updatedInfo);
       res.send(result);
       console.log(result);
-
-    })
+    });
     // delete a specific orderd food
-    app.delete('/api/v1/user/deletefood/:id', logger, async(req, res) => {
+    app.delete("/api/v1/user/deletefood/:id", logger, async (req, res) => {
       const foodId = req.params.id;
-      const query = {_id: new ObjectId(foodId)};
+      const query = { _id: new ObjectId(foodId) };
       const result = await purchaseFoodCollection.deleteOne(query);
       res.send(result);
-    })
+    });
     // delete a specific added food item
-    app.delete('/api/v1/route/myaddedfood/:id', async(req, res) => {
+    app.delete("/api/v1/route/myaddedfood/:id", async (req, res) => {
       const foodId = req.params.id;
-      const query = {_id: new ObjectId(foodId)};
+      const query = { _id: new ObjectId(foodId) };
       const result = await allFoodCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
